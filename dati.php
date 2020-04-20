56bc1652fb09c73efc5f4a8c5bff077931b8cf1c
Jasmin-Mediani <molihua1992@gmail.com>
2020-04-20 18:06:49 +0200

    creato file dati.php con array di array

---
+++ dati.php
@@ -0,0 +1,48 @@
+<?php
+    $cards = [
+        [
+            'immagine' => 'https://www.bonjovimusic.it/upload/album/4/new-jersey-medium.jpg',
+            "titolo" => "New Jersey",
+            "autore" => "Bon Jovi",
+            "anno" => "1992"
+        ],
+        [
+            'immagine' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._AC_SX522_.jpg',
+            'titolo' => 'Live At Wembley 86 ',
+            'autore' => 'Queen',
+            'anno' => '1992'
+        ],
+        [
+            'immagine' => 'https://m.media-amazon.com/images/I/81r5dWQRBJL._SS500_.jpg',
+            'titolo' => "Tens Summoners Tale",
+            'autore' => "Sting",
+            'anno' => "1993"
+
+        ],
+        [
+            'immagine' => "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg", 'alt="">',
+            'titolo' => " Steve Gadd Band ",
+            'autore' =>  "Steve Gadd Band ",
+            'anno' => "2018"
+
+        ],
+
+        [
+            'immagine' => "https://images-na.ssl-images-amazon.com/images/I/81ll31VxLDL._AC_SX466_.jpg",
+            'titolo' => "Brave New World",
+            'autore' => "Iron Maiden",
+            'anno' => "2000",
+
+        ],
+
+        [
+            'immagine' => "https://images-na.ssl-images-amazon.com/images/I/81lR-Qe4X2L._AC_SX466_.jpg",
+            'titolo' => "One more car, one more rider",
+            'autore' => "Eric Clapton",
+            'anno' => "2002"
+
+        ],
+
+    ];
+
+?>
